b3b8fa9348a77c0e74fbfac46eda45477a05ca1b
Bekter <besedov347@gmail.com>
2023-05-20 18:26:24 +0300

    lesson1

---
+++ lessons1/assets/js/main.js
@@ -0,0 +1,34 @@
+// document.write("Hello I am Javascript");
+// alert("Hello I am Javascript");
+// console.log("Hello I am Javascript");
+
+// hello="Hi I am Java"; type string
+
+// isEmpty=false; type boolean
+
+// x=23; type number
+
+// x = 12;
+// y = 5;
+// t = x + y;
+// document.write("toplama:" + t + "<br/>");
+// c = x - y;
+// document.write("chixma:" + c + "<br/>");
+// v = x * y;
+// document.write("chixma:" + v + "<br/>");
+// q = x % y;
+// document.write("qaliq:" + q + "<br/>");
+// tam = x / y;
+// document.write("tam qaliq:" + tam + "<br/>");
+
+// d=5; //5
+// d=d+2; //d=5+2; 7
+// d=5;
+// d+=26;
+// d = 5;
+// d = d + 1;
+// d += 1;
+// d++;
+// d--;
+// document.write(d);
+
