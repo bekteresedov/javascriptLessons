f01093e3f868a2dc2b4adc96017bfc477d13ebe0
Bekter <besedov347@gmail.com>
2023-05-24 17:52:16 +0300

    lessons2 added

---
+++ lessons2/main.js
@@ -0,0 +1,51 @@
+// document.write("Hello world");
+// console.log("Hello world");
+// alert("Hello world");
+// a = 4; //8
+// b = 8; //4
+// a = a + b; //12
+// b = a - b; //4
+// a=a-b;
+// console.log(b);
+// console.log(a);
+
+// a = 5;
+// c = 6;
+// result = a==c;
+// console.log(result);
+
+// a = 5;
+// c = 6;
+// if (a == c) {
+//   console.log("a c ye beraberdir");
+// } else if (a > c) {
+//   console.log("a c den boyukdur");
+// }
+
+eded1 = 15;
+eded2 = 10;
+operator = "-";
+result = 0;
+if (operator == "+") {
+  result = eded1 + eded2;
+  console.log(eded1 + " + " + eded2 + "=" + result);
+} else if (operator == "-") {
+  result = eded1 - eded2;
+  console.log(eded1 + " - " + eded2 + "=" + result);
+} else if (operator == "*") {
+  result = eded1 * eded2;
+  console.log(eded1 + " * " + eded2 + "=" + result);
+} else if (operator == "/") {
+  result = eded1 / eded2;
+  console.log(eded1 + " / " + eded2 + "=" + result);
+} else {
+  alert("Zehmet olmasa operatoru duzgun daxil edin!");
+}
+
+// bunun ardin tamamlayin kubun kvadratin
+
+// heftenin gunun tapan proqram
+
+//sait yoxsa samit
+
+//ededin 2 yue bolunub bolunmemesin
