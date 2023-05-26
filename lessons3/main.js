fb089a914471bf87fda1e803afa2fb0cdcde29d4
Bekter <besedov347@gmail.com>
2023-05-26 17:48:48 +0300

    added lessons3

---
+++ lessons3/main.js
@@ -0,0 +1,28 @@
+// eded = 10;
+// if (eded % 2 == 0 && eded % 5 == 0 && eded % 6 == 0) {
+//   console.log("bu eded 2 e ve 5 e 6 e tam bolunur");
+// } else {
+//   console.log("bu eded 2 e ve 5 e tam bolunmur");
+// }
+// eded = 21;
+// if (eded % 2 == 0 || eded % 7 == 0 || eded % 6 == 0) {
+//   console.log("bu eded 2 e ve 5 e 6 e tam bolunur");
+// } else {
+//   console.log("bu eded 2 e ve 5 e tam bolunmur");
+// }
+
+// eded = 12;
+// if (!(eded % 2 == 0)) {
+//   console.log("bu eded 2 ye bolunmur");
+// } else {
+//   console.log("bu eded 2 ye bolunur");
+// }
+// alma = false;
+// if (alma == false) {
+//   console.log("Alma");
+// }else {
+//   console.log("Armud");
+// }
+// a = 6;
+// b = 4;
+// a > b ? console.log("boyukdur abi") : console.log("Boyuk deyil");
