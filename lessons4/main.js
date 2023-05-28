a01e0222eb3a58f247f63119c0fde0b3de598e2f
Bekter <besedov347@gmail.com>
2023-05-28 20:00:26 +0300

    lessons 4 added

---
+++ lessons4/main.js
@@ -0,0 +1,40 @@
+// var a = 12;
+// var b = 81;
+// a > b
+//   ? console.log("Boyukdur") //true
+//   : a == b
+//   ? log("beraberdur")
+//   : console.log("kicikdir");
+
+// var students = ["Tehmasib", "Kanan", "Orxan", "3"];
+// console.log(students[1]);
+// console.log(students[1]);
+// console.log(students[1]);
+
+// console.log(students.length);
+
+// for (var i = 0; i < 100; i++) {
+//   //   console.log(students[3]);
+//   console.log("hello world");
+// }
+
+// for (var i = 100; i > 0; i--) {
+//   console.log(i);
+// }
+
+// vurma cedvelin ekranda cixardaciq
+
+// 1 den 100 e qeder  2ye bolunen ededleri ver
+// 5 elementli array olacaq sondan 3 elementin chap olunmasin isteyirem
+
+// continue
+// break
+
+// for (var i = 1; i <= 10; i++) {
+//   console.log(i);
+//   if (i == 4) {
+//     continue;
+//   } else {
+//     break;
+//   }
+// }
